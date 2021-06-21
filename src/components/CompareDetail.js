45f663c0f6c9242ed491637acd462eed2a3202be
peggy1008123 <peggy1008123@yahoo.com.tw>
2021-06-21 15:25:31 +0800

    compare-rwd finished

--- src/components/CompareDetail.js
+++ src/components/CompareDetail.js
@@ -9,10 +9,7 @@ import { useState } from "react";
 import { Row, Col, Select } from "antd";
 const { Option } = Select;
 
-
-
 export default function CompareDetail() {
-
   const chooseJSON = (value) => {
     switch (value) {
       case "chickOne":
@@ -28,12 +25,12 @@ export default function CompareDetail() {
       default:
         return 0;
     }
-  }
+  };
 
   const [chosenItem1, setChosenItem1] = useState(chickOne);
-  const [ chosenItem2, setChosenItem2] = useState(chickTwo);
-  const [ chosenItem3, setChosenItem3] = useState(chickThree);
-  
+  const [chosenItem2, setChosenItem2] = useState(chickTwo);
+  const [chosenItem3, setChosenItem3] = useState(chickThree);
+
   function selectOne(value) {
     console.log(`selected ${value}`);
     console.log(chooseJSON(value));
@@ -55,61 +52,61 @@ export default function CompareDetail() {
   return (
     <>
       <Row>
-        <h1 className="compare-title">
-          比較麻將桌機型
-        </h1>
+        <h1 className="compare-title">比較麻將桌機型</h1>
       </Row>
       <Row>
-        <p className="compare-little-text">
-          機型比較讓你一目了然免煩惱
-        </p>
+        <p className="compare-little-text">機型比較讓你一目了然免煩惱</p>
       </Row>
       <hr className="compare-line"></hr>
-      <Row>
-        <Col>
-          <Select
-            id="selectOne"
-            defaultValue="chickOne"
-            onChange={selectOne}
-          >
-            <Option value="chickOne">一吉郎</Option>
-            <Option value="chickTwo">二吉郎</Option>
-            <Option value="chickThree">三吉郎</Option>
-            <Option value="chickFour">四吉郎</Option>
-            <Option value="chickFive">五吉郎</Option>
-          </Select>
-          <CompareItem chosenItem={chosenItem1} />
-        </Col>
-        <Col>
-          <Select
-            id="selectOne"
-            onChange={selectTwo}
-            defaultValue="chickTwo"
-          >
-            <Option value="chickOne">一吉郎</Option>
-            <Option value="chickTwo">二吉郎</Option>
-            <Option value="chickThree">三吉郎</Option>
-            <Option value="chickFour">四吉郎</Option>
-            <Option value="chickFive">五吉郎</Option>
-          </Select>
-          <CompareItem chosenItem={chosenItem2} />
-        </Col>
-        <Col>
-          <Select 
-          id="selectOne"
-          onChange={selectThree}
-          defaultValue="chickThree"
-          >
-            <Option value="chickOne">一吉郎</Option>
-            <Option value="chickTwo">二吉郎</Option>
-            <Option value="chickThree">三吉郎</Option>
-            <Option value="chickFour">四吉郎</Option>
-            <Option value="chickFive">五吉郎</Option>
-          </Select>
-          <CompareItem chosenItem={chosenItem3} />
-        </Col>
-      </Row>
-
+      <div className="compare-outside">
+        <Row>
+          <Col className="compare-space">
+            <Select
+              className="compare-select"
+              id="selectOne"
+              defaultValue="chickOne"
+              onChange={selectOne}
+            >
+              <Option value="chickOne">一吉郎</Option>
+              <Option value="chickTwo">二吉郎</Option>
+              <Option value="chickThree">三吉郎</Option>
+              <Option value="chickFour">四吉郎</Option>
+              <Option value="chickFive">五吉郎</Option>
+            </Select>
+            <CompareItem chosenItem={chosenItem1} />
+          </Col>
+          <Col className="compare-space">
+            <Select
+              className="compare-select"
+              id="selectOne"
+              onChange={selectTwo}
+              defaultValue="chickTwo"
+            >
+              <Option value="chickOne">一吉郎</Option>
+              <Option value="chickTwo">二吉郎</Option>
+              <Option value="chickThree">三吉郎</Option>
+              <Option value="chickFour">四吉郎</Option>
+              <Option value="chickFive">五吉郎</Option>
+            </Select>
+            <CompareItem chosenItem={chosenItem2} />
+          </Col>
+          <Col className="compare-space" id="selectthree">
+            <Select
+              className="compare-select"
+              id="selectOne"
+              onChange={selectThree}
+              defaultValue="chickThree"
+            >
+              <Option value="chickOne">一吉郎</Option>
+              <Option value="chickTwo">二吉郎</Option>
+              <Option value="chickThree">三吉郎</Option>
+              <Option value="chickFour">四吉郎</Option>
+              <Option value="chickFive">五吉郎</Option>
+            </Select>
+            <CompareItem chosenItem={chosenItem3} />
+          </Col>
+        </Row>
+      </div>
     </>
   );
 }
--- src/components/CompareItem.js
+++ src/components/CompareItem.js
@@ -1,21 +1,25 @@
+import { Link } from "react-router-dom"
+
 export default function CompareItem({ chosenItem }) {
     return (
         <div style={{display: "grid"}}> 
-            <img src={chosenItem[0].image} />
-            <img src={chosenItem[0].compareImg1} />
-            <img src={chosenItem[0].compareImg2} />
-            <img src={chosenItem[0].compareImg3} />
-            <img src={chosenItem[0].compareImg4} />
-            <img src={chosenItem[0].compareImg5} />
-            <img src={chosenItem[0].compareImg6} />
-            <img src={chosenItem[0].compareImg7} />
-            <img src={chosenItem[0].compareImg8} />
-            <img src={chosenItem[0].compareImg9} />
-            <img src={chosenItem[0].compareImg10} />
-            <img src={chosenItem[0].compareImg11} />
-            <img src={chosenItem[0].compareImg12} />
-            <img src={chosenItem[0].compareImg13} />
-            <img src={chosenItem[0].compareImg14} />
+            <img className="table-img" src={chosenItem[0].image} />
+            <div className="compare-price">NTD{chosenItem[0].price}</div>
+            <Link className="compare-btn" to={`/detail/${chosenItem[0].link}`}>點我購買</Link>
+            <img className="feature-img" src={chosenItem[0].compareImg1} />
+            <img className="feature-img" src={chosenItem[0].compareImg2} />
+            <img className="feature-img" src={chosenItem[0].compareImg3} />
+            <img className="feature-img" src={chosenItem[0].compareImg4} />
+            <img className="feature-img" src={chosenItem[0].compareImg5} />
+            <img className="feature-img" src={chosenItem[0].compareImg6} />
+            <img className="feature-img" src={chosenItem[0].compareImg7} />
+            <img className="feature-img" src={chosenItem[0].compareImg8} />
+            <img className="feature-img" src={chosenItem[0].compareImg9} />
+            <img className="feature-img" src={chosenItem[0].compareImg10} />
+            <img className="feature-img" src={chosenItem[0].compareImg11} />
+            <img className="feature-img" src={chosenItem[0].compareImg12} />
+            <img className="feature-img" src={chosenItem[0].compareImg13} />
+            <img className="feature-img" src={chosenItem[0].compareImg14} />
         </div>
     )
 }
